8096895d742b4d6c2f9e582a7be4c5b3272f8d12
Brayan Obispo Torres <brayanobisto.dev@gmail.com>
2025-03-28 19:52:15 -0500

    Enhance beer detail view and tab layout
    - Updated `beer/[id].tsx` to fetch and display beer details, including image, name, brand, price, and stock.
    - Improved tab layout in `_layout.tsx` by adjusting tab bar item styles and increasing icon sizes for better visibility.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,6 @@
 import { Tabs } from "expo-router";
 import colors from "tailwindcss/colors";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import tailwindConfig from "@/tailwind.config";
-import resolveConfig from "tailwindcss/resolveConfig";
-
-const fullConfig = resolveConfig(tailwindConfig);
 
 export default function TabsLayout() {
   return (
@@ -13,8 +9,15 @@ export default function TabsLayout() {
         headerShown: false,
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[500],
+        tabBarItemStyle: {
+          flexDirection: "row",
+          alignItems: "center",
+        },
         tabBarStyle: {
           backgroundColor: colors.white,
+          height: 75,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
         },
       }}
     >
@@ -23,7 +26,7 @@ export default function TabsLayout() {
         options={{
           tabBarLabel: "Inicio",
           tabBarIcon: ({ focused }) => (
-            <FontAwesome name="home" size={24} color={focused ? colors.green[500] : colors.gray[500]} />
+            <FontAwesome name="home" size={27} color={focused ? colors.green[500] : colors.gray[500]} />
           ),
         }}
       />
@@ -32,7 +35,7 @@ export default function TabsLayout() {
         options={{
           tabBarLabel: "Pedidos",
           tabBarIcon: ({ focused }) => (
-            <FontAwesome name="file-text" size={24} color={focused ? colors.green[500] : colors.gray[500]} />
+            <FontAwesome name="file-text" size={27} color={focused ? colors.green[500] : colors.gray[500]} />
           ),
         }}
       />
@@ -42,7 +45,7 @@ export default function TabsLayout() {
           tabBarLabel: "Carrito",
 
           tabBarIcon: ({ focused }) => (
-            <FontAwesome name="shopping-cart" size={24} color={focused ? colors.green[500] : colors.gray[500]} />
+            <FontAwesome name="shopping-cart" size={27} color={focused ? colors.green[500] : colors.gray[500]} />
           ),
         }}
       />
--- app/beer/[id].tsx
+++ app/beer/[id].tsx
@@ -1,9 +1,51 @@
-import { View, Text } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { useLocalSearchParams } from "expo-router";
 
+interface Beer {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  imageUrl: string;
+  stock: number;
+}
 export default function Beer() {
+  const [beer, setBeer] = useState<Beer>();
+  const { id } = useLocalSearchParams();
+
+  console.log(id);
+  console.log(beer);
+
+  useEffect(() => {
+    const fetchBeers = async () => {
+      const { data, error } = await supabase.from("beers").select("*").eq("id", id).single();
+
+      if (error) {
+        console.error("Error fetching beers:", error);
+      } else {
+        setBeer(data as unknown as Beer);
+      }
+    };
+    fetchBeers();
+  }, []);
+
   return (
-    <View>
-      <Text>Beer</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView>
+        <Image source={{ uri: beer?.imageUrl }} className="aspect-square w-full mb-4" />
+        <View className="p-4">
+          <Text className="text-2xl font-bold">{beer?.name}</Text>
+          <Text className="text-gray-500">{beer?.brand}</Text>
+          <Text className="text-green-500">{beer?.price}</Text>
+          <Text className="text-gray-500">{beer?.stock}</Text>
+          <TouchableOpacity className="bg-green-500 p-2 rounded-md">
+            <Text className="text-white text-center">Agregar al carrito</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
