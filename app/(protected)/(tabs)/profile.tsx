a11497d6b7b79d4f2c26f08fa161c02bf9f85854
Brayan Obispo Torres <brayanobisto.dev@gmail.com>
2025-03-29 01:04:48 -0500

    Implement user authentication and sign-out functionality
    - Added user authentication logic in `_layout.tsx` to redirect users based on their authentication status.
    - Removed redundant loading state management from `index.tsx` and streamlined the login process.
    - Introduced a sign-out feature in the `Profile` component, allowing users to log out and redirecting them to the home screen.

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "expo-router";
 
 export default function Profile() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error al cerrar sesión:", error);
+    } else {
+      router.replace("/");
+    }
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-1 justify-center items-center">
-        <Text>Profile</Text>
-      </View>
+      <TouchableOpacity className="p-4" onPress={handleSignOut}>
+        <Text>Salir</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,31 @@
-import { Fragment } from "react";
-import { Stack } from "expo-router";
+import { Fragment, useEffect, useState } from "react";
+import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 import "@/global.css";
 import { ActivityIndicator } from "react-native";
+import { supabase } from "@/lib/supabase";
 
 export default function RootLayout() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user) {
+        router.replace("/(protected)/(tabs)");
+      } else {
+        router.replace("/");
+      }
+
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <Fragment>
       <StatusBar style="inverted" />
