cbf218b9a1585c751d0299950685e7c2ac543bf9
Brayan Obispo Torres <brayanobisto.dev@gmail.com>
2025-03-30 13:07:07 -0500

    Add SafeAreaView component for improved layout management
    - Introduced a new `SafeAreaView` component in `components/ui/SafeAreaView.tsx` to manage safe area insets for better layout across devices.
    - Utilized `react-native-safe-area-context` for handling safe areas and integrated a utility for dynamic class name management with Tailwind CSS.
    - Enhanced code reusability and organization by encapsulating safe area handling in a dedicated component.

---
+++ components/ui/SafeAreaView.tsx
@@ -0,0 +1,17 @@
+import type { SafeAreaViewProps } from "react-native-safe-area-context";
+import { memo } from "react";
+import { SafeAreaView as RN_SafeAreaView } from "react-native-safe-area-context";
+
+import { cn } from "@/utils/cn";
+
+export const SafeAreaView = memo((props: SafeAreaViewProps) => {
+  const { children, className, ...restProps } = props;
+
+  return (
+    <RN_SafeAreaView className={cn("flex-1 bg-white p-4", className)} {...restProps}>
+      {children}
+    </RN_SafeAreaView>
+  );
+});
+
+SafeAreaView.displayName = "SafeAreaView";
