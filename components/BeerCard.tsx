5f944f671740657a1fdec81ca3ae27ea377a4828
Brayan Obispo Torres <brayanobisto.dev@gmail.com>
2025-03-30 01:04:33 -0500

    Refactor BeerCard component to use useRouter for navigation
    - Replaced the Link component with useRouter for improved routing functionality.
    - Wrapped the card in a TouchableOpacity for better touch feedback and interaction.
    - Maintained the existing layout and styling while enhancing the navigation experience.

--- components/BeerCard.tsx
+++ components/BeerCard.tsx
@@ -1,5 +1,5 @@
 import { FC, memo } from "react";
-import { Link } from "expo-router";
+import { useRouter } from "expo-router";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { Tables } from "@/types/supabase";
 
@@ -10,35 +10,35 @@ interface BeerCardProps {
 }
 
 export const BeerCard: FC<BeerCardProps> = memo(({ beer }) => {
-  return (
-    <Link
-      href={{
-        pathname: "/(protected)/beer/[id]",
-        params: {
-          id: beer.id,
-        },
-      }}
-      asChild
-    >
-      <TouchableOpacity
-        className={`flex-1 rounded-xl bg-white shadow-md shadow-black/60 overflow-hidden flex-row h-[${CARD_HEIGHT}px]`}
-      >
-        <Image source={{ uri: beer.imageUrl }} className="aspect-square" height={CARD_HEIGHT} width={CARD_HEIGHT} />
-        <View className="flex-1 py-4 px-4 border-l border-gray-200">
-          <Text className="text-xl font-bold flex-shrink" numberOfLines={2}>
-            {beer.name}
-          </Text>
-          <Text className="text-gray-500">{beer.brand}</Text>
-          <Text className="text-sm">{beer.stock} unidades disponibles</Text>
+  const router = useRouter();
 
-          <Text className="text-xl font-bold text-green-500">
-            {Intl.NumberFormat("es-CO", { style: "currency", currency: "COP", minimumFractionDigits: 0 }).format(
-              Number(beer.price)
-            )}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    </Link>
+  return (
+    <TouchableOpacity
+      className={`flex-1 rounded-xl bg-white shadow-md shadow-black/60 overflow-hidden flex-row h-[${CARD_HEIGHT}px]`}
+      onPress={() =>
+        router.push({
+          pathname: "/(protected)/beer/[id]",
+          params: {
+            id: beer.id,
+          },
+        })
+      }
+    >
+      <Image source={{ uri: beer.imageUrl }} className="aspect-square" height={CARD_HEIGHT} width={CARD_HEIGHT} />
+      <View className="flex-1 py-4 px-4 border-l border-gray-200">
+        <Text className="text-xl font-bold flex-shrink" numberOfLines={2}>
+          {beer.name}
+        </Text>
+        <Text className="text-gray-500">{beer.brand}</Text>
+        <Text className="text-sm">{beer.stock} unidades disponibles</Text>
+
+        <Text className="text-xl font-bold text-green-500">
+          {Intl.NumberFormat("es-CO", { style: "currency", currency: "COP", minimumFractionDigits: 0 }).format(
+            Number(beer.price)
+          )}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 });
 
