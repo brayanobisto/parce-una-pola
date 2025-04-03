591d9ee9de70f8e8b727a0ef4aa4666fb466c5cd
Brayan Obispo Torres <brayanobisto.dev@gmail.com>
2025-04-03 00:07:53 -0500

    Add getCurrentUser function to auth module for user retrieval
    - Implemented a new `getCurrentUser` function in the `auth.ts` file to fetch the current authenticated user from Supabase, improving user session management.
    - Added error handling to ensure proper response in case of retrieval failure, enhancing robustness of the authentication flow.

--- lib/supabase/auth.ts
+++ lib/supabase/auth.ts
@@ -1,5 +1,18 @@
 import { supabase } from "@/lib/supabase";
 
+export const getCurrentUser = async () => {
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error) {
+    throw error;
+  }
+
+  return user;
+};
+
 export const signInAnonymously = async (name: string) => {
   const { error: signInError } = await supabase.auth.signInAnonymously();
 
